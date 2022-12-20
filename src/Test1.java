bdb733ab3ae4aa5b740c176234972c14b0108978
Gulsah Tosun <gulsahustatosun@gmail.com>
2022-12-20 14:55:18 +0000

    Wrote Havva's branch

--- src/Test1.java
+++ src/Test1.java
@@ -1,6 +1,7 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Havva Demirel updated");
+        System.out.println("I wrote on Havva's branch");
     }
 
 
