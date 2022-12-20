578239f00129d49fce71bda7c267e5732ce44f20
Selcuk Cellik <sel2014@hotmail.com>
2022-12-20 23:04:35 +0000

    remove duplicates from String

---
+++ src/RemoveDuplicates.java
@@ -0,0 +1,18 @@
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        String name = "Muuhhhhttaaaar";
+    }
+
+
+
+
+}
+/*
+create a method that can remove duplicates from
+any given String.
+input = "Muuhhhhttaaaar"
+output = "Muhtar"
+
+ */
