2a2daec484cadf69ac609f7e4a65056faff10b9d
Selcuk Cellik <sel2014@hotmail.com>
2022-12-20 23:15:46 +0000

    remove duplicates completed

--- src/RemoveDuplicates.java
+++ src/RemoveDuplicates.java
@@ -3,14 +3,27 @@ public class RemoveDuplicates {
     public static void main(String[] args) {
 
         String name = "Muuhhhhttaaaar";
+        System.out.println(removeDuplicates(name));
     }
 
+    public static String removeDuplicates(String str){
 
+        String nonDuplicates = "";
 
+        char[] chars = str.toCharArray();
+
+        for (char eachChar : chars) {
+            if (!nonDuplicates.contains(""+eachChar)){
+                nonDuplicates+=eachChar;
+            }
+        }
+        return nonDuplicates;
+
+    }
 
 }
 /*
-create a method that can remove duplicates from
+Create a method that can remove duplicates from
 any given String.
 input = "Muuhhhhttaaaar"
 output = "Muhtar"
